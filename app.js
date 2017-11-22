8aa7512b7f594bd1f27ed644946d38a79ead917d
John <jpkarvonen@gmail.com>
2017-11-21 19:43:00 -0500

    Assignement 7 completed

--- app.js
+++ app.js
@@ -19,19 +19,36 @@ function onReady() {
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    // create a button
+    let removeItem = document.createElement('button')
+
+    // set the button's type to button
+    removeItem.type = "button";
+
+    // set the button's textContent
+    removeItem.textContent = "Remove"
+
     // set the title
     newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    // attach remove button to li
+
+    newLi.appendChild(removeItem);
+
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     // empty the input
     newToDoText.value = '';
-  });
 
+    // add function to remove button
+    removeItem.addEventListener('click', event => {
+      removeItem.closest('li').remove();
+    });
+  });
 }
 
 
