5258d7fe5ea713854cad93263d2abd20946c2242
John <jpkarvonen@gmail.com>
2017-11-22 18:33:00 -0500

    modify app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 function onReady() {
   const toDos = [];
   const addTodoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
+
     if (!newToDoText.value) { return; }
+
+    // increment id variable
+    id++;
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id.value
     });
     newToDoText.value = '';
 
@@ -20,6 +26,11 @@ function onReady() {
 
     toDoList.textContent = '';
 
+    // create delete button
+    let removeItem = document.createElement('button')
+    removeItem.type = "button";
+    removeItem.textContent = "Remove";
+
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
@@ -28,6 +39,20 @@ function onReady() {
       newLi.textContent = toDo.title;
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+    });
+
+    // attach the delete button to li
+    newLi.appendChild(removeItem);
+
+    // add function to delete button
+    removeItem.addEventListener('click', event => {
+
+      toDos.filter(function(toDo){
+        return toDo.id !== toDo.id;
+      });
+
+      renderTheUI();
+
     });
   }
 
